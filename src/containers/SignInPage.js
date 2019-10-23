6b4ced52ce3aaa387ae81f48adf53bbd755192e2
colinhalexander <chalexander@icloud.com>
2019-10-23 13:44:03 -0600

    add sign-up post to backend

--- src/containers/SignInPage.js
+++ src/containers/SignInPage.js
@@ -4,10 +4,16 @@ import { Link } from 'react-router-dom'
 export default class SignInPage extends Component {
 
   state = {
-    firstName: "",
-    lastName: "",
+    firstname: "",
+    lastname: "",
     username: "",
     password: ""
+  }
+
+  static defaultProps = {
+    location: {
+      existingUser: false
+    }
   }
 
   toggleExistingUser = () => {
@@ -21,8 +27,8 @@ export default class SignInPage extends Component {
   addNamesToForm = () => {
     return (
       <span id="form-names">
-        <input onChange={this.handleChange} type="text" name="firstName" placeholder="First Name" />
-        <input onChange={this.handleChange} type="text" name="lastName" placeholder="Last Name" />
+        <input onChange={this.handleChange} type="text" name="firstname" placeholder="First Name" />
+        <input onChange={this.handleChange} type="text" name="lastname" placeholder="Last Name" />
       </span>
     )
   }
@@ -35,18 +41,44 @@ export default class SignInPage extends Component {
 
   handleSubmit = (event) => {
     event.preventDefault()
+
+    const request = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(this.state)
+    }
+
+    this.props.location.existingUser 
+      ? this.logInUser(request) 
+      : this.createNewUser(request) 
   }
+  
+  createNewUser = (request) => {
+    fetch("http://localhost:3000/users", request)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => console.log(error))
+  }
+
+  // logInUser = (request) => {
+
+  // }
 
   render() {
     return (
       <div className="sign-in-wrapper">
         <div className="sign-in">
           <h3>{this.props.location.existingUser ? "Log In" : "Sign Up"}</h3>
-          <form className="sign-in-form">
+          <form className="sign-in-form" onSubmit={this.handleSubmit}>
             {!this.props.location.existingUser ? this.addNamesToForm() : ""}
-            <input onChange={this.handleChange} type="text" name="username" placeholder="Username" />
+            <input onChange={this.handleChange} type="text" name="username" placeholder="Username"/>
             <input onChange={this.handleChange} type="password" name="password" placeholder="Password"/>
-            <input id="submit" type="submit" />
+            <input id="submit" type="submit"/>
           </form>
           {this.toggleExistingUser()}
         </div>
